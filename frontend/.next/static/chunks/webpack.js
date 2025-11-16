5fca0f6cbfe6024c32807077fc2cf2af3269c8e3
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2025-11-16 17:14:08 +0000

    update

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fb6dd3b992f07897"; }
+/******/ 		__webpack_require__.h = function() { return "5f55533d1b4ffea1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
