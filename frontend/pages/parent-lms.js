dfb319808b1da319a2f2d9ae821fe4f8721b35b4
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2026-08-26 16:06:56 +0000

    Require authentication for parent LMS portal

--- frontend/pages/parent-lms.js
+++ frontend/pages/parent-lms.js
@@ -1,2 +1,9 @@
 import LmsPortal from '../components/LmsPortal'
+
+export async function getServerSideProps({ req }) {
+  const user = (typeof req.getUser === 'function' && req.getUser()) || req.session?.user
+  if (!user) return { redirect: { destination: '/login?next=/parent-lms', permanent: false } }
+  return { props: {} }
+}
+
 export default function ParentLms(){ return <LmsPortal mode="parent" /> }
