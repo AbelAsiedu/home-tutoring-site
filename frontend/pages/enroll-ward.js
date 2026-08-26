d5eadc37553ccd43355eb5f58252c3c509d4c891
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2026-08-26 16:07:01 +0000

    Require authentication for ward enrollment

--- frontend/pages/enroll-ward.js
+++ frontend/pages/enroll-ward.js
@@ -1,2 +1,9 @@
 import LmsPortal from '../components/LmsPortal'
+
+export async function getServerSideProps({ req }) {
+  const user = (typeof req.getUser === 'function' && req.getUser()) || req.session?.user
+  if (!user) return { redirect: { destination: '/login?next=/enroll-ward', permanent: false } }
+  return { props: {} }
+}
+
 export default function EnrollWard(){ return <LmsPortal mode="enroll" /> }
